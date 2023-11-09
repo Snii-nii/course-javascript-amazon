375eaa1ab3a7815e140a0e90de7a5281774dcc07
Nevaehel <sonja.hirvonen@gmail.com>
2023-11-09 11:54:44 +0200

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5, 
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4, 
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5, 
-    count: 56
-  },
-  priceCents: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => { //Accumulator pattern
@@ -72,7 +46,7 @@ products.forEach((product) => { //Accumulator pattern
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -80,3 +54,36 @@ products.forEach((product) => { //Accumulator pattern
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+        cart.forEach((item) => {
+          if (productId === item.productId) {
+            matchingItem = item;
+          }
+        });
+
+        if (matchingItem) {
+          matchingItem.quantity += 1;
+        } else {
+          cart.push({
+            productId: productId,
+            quantity: 1
+          });
+        }
+
+      let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+    });
+  });
